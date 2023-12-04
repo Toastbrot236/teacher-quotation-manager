eaa1f50f858e6107cf0fb3c7a59b915432661881
IllusioQuest <illusioquest@gmail.com>
2023-12-04 20:20:37 +0100

    Enable PWA

--- src/main/java/com/abi/quotes/Application.java
+++ src/main/java/com/abi/quotes/Application.java
@@ -2,6 +2,7 @@ package com.abi.quotes;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "zitate-sammlung")
+@PWA(name = "Zitate-Sammlung für das Abi 2026", shortName = "Abi-Zitate")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
