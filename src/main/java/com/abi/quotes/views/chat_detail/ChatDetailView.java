Toastbrot236/teacher-quotation-manager

commit 4f9ca747de691c11b8d4128e90bc7dedf2fc4194

Preparations for #2, needs live testing. Also fixed #40

frontend/themes/zitate-sammlung/schulmanager.css
@@ -178,8 +178,8 @@ span.origi, span.origi > b {
 
 .header-top {
   background-color: var(--lumo-base-color);
-  position: sticky;
-  position: -webkit-sticky;
+  position: fixed;
+  position: -webkit-fixed;
   top: 0;
   width: 110%;
   z-index: 10;

src/main/java/com/abi/quotes/views/chat_detail/ChatDetailView.java
@@ -1,8 +1,14 @@
 package com.abi.quotes.views.chat_detail;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
@@ -12,6 +18,7 @@ import com.abi.quotes.views.MainLayout;
 import com.abi.quotes.views.beta_test.SmView;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.UI;
@@ -72,6 +79,8 @@ public class ChatDetailView extends SmView implements HasUrlParameter<String> {
 	private Upload upload;
 	
 	private VerticalLayout innerLayout;
+	
+	private ArrayList<String> uploadedFiles;
 	
 	//@Override
 	public void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
@@ -204,7 +213,12 @@ public class ChatDetailView extends SmView implements HasUrlParameter<String> {
 		innerLayout.addComponentAsFirst(dateDisplay);
 		innerLayout.setHorizontalComponentAlignment(Alignment.CENTER, dateDisplay);
 		
+		Div bottomAnchor = new Div();
+		innerLayout.add(bottomAnchor);;
+		
 		add(innerLayout);
+		
+		bottomAnchor.scrollIntoView();
 		
 	}
 	
@@ -271,7 +285,7 @@ public class ChatDetailView extends SmView implements HasUrlParameter<String> {
 			attachmentButton.addClickListener(e -> {
 				toggleUpload(outerContainer);
 			});
-			attachmentButton.setEnabled(false); //TODO
+			attachmentButton.setEnabled(true); //TODO
 			
 			TextArea area = new TextArea();
 			area.setWidthFull();
@@ -307,6 +321,8 @@ public class ChatDetailView extends SmView implements HasUrlParameter<String> {
 	private void toggleUpload(VerticalLayout container) {
 		
 		if (upload == null) {
+			
+			uploadedFiles = new ArrayList<String>();
 		
 			/*Dialog uploadDialog = new Dialog();
 			uploadDialog.setHeaderTitle("Anhänge hochladen");
@@ -318,6 +334,20 @@ public class ChatDetailView extends SmView implements HasUrlParameter<String> {
 			MultiFileBuffer buffer = new MultiFileBuffer();
 			Upload uploadComponent = new Upload(buffer);
 			uploadComponent.setI18n(getUploadI18n());
+			
+			add(new Html("<style>vaadin-upload-file {padding: 0;}y/style>"));
+			
+			//File successfully uploaded
+			uploadComponent.addSucceededListener(e -> {
+				uploadedFiles.add(e.getFileName());
+			});
+			//File removed via clear button
+			uploadComponent.getElement().addEventListener("file-remove", event -> {
+				  elemental.json.JsonObject eventData = event.getEventData();
+				  uploadedFiles.remove(eventData.getString("event.detail.file.name"));
+				  for (String s : uploadedFiles) System.out.println(s);
+				}).addEventData("event.detail.file.name");
+			
 	
 			/*Button uploadButton = new Button("Anhängen", e -> {
 				handleUploads(buffer);
@@ -341,10 +371,6 @@ public class ChatDetailView extends SmView implements HasUrlParameter<String> {
 			upload = null;
 		}
 		
-	}
-	
-	private void handleUploads(MultiFileBuffer buffer) {
-		//buffer.
 	}
 
 	protected void initialise() {}

src/main/java/com/abi/quotes/views/start/StartView.java
@@ -8,6 +8,7 @@ import com.abi.quotes.views.profil.ProfilView;
 import com.abi.quotes.views.pushtest.PushTestView;
 import com.abi.quotes.views.student_quote.StudentQuoteView;
 import com.abi.quotes.views.teacher.TeacherView;
+import com.abi.quotes.views.uploadtest.FileProcessorView;
 import com.abi.quotes.views.users.UsersView;
 import com.abi.quotes.views.zitate.ZitateView;
 import com.vaadin.flow.component.Component;
@@ -174,7 +175,7 @@ public class StartView extends VerticalLayout implements HasHelp {
         Button nutzerverwaltungButton = createNavigationButton("Nutzerverwaltung", UsersView.class);
         nutzerverwaltungButton.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
         
-        Button testsButton = createNavigationButton("Neues Zeug testen", PushTestView.class);
+        Button testsButton = createNavigationButton("Neues Zeug testen", FileProcessorView.class);
         testsButton.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
 
         VerticalLayout buttonsLayout = new VerticalLayout(firstRow, new HorizontalLayout(zitateNachLehrerButton, schuelerZitateButton), newQuoteButton);

src/main/java/com/abi/quotes/views/uploadtest/FileProcessorView.java
@@ -1,42 +1,91 @@
 package com.abi.quotes.views.uploadtest;
 
 import com.vaadin.flow.component.button.Button;
+
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.abi.quotes.views.MainLayout;
+import com.abi.quotes.views.chat_detail.ChatDetailView;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.component.upload.receivers.MultiFileBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoIcon;
+
+import schulmanager.api.UploadFileRequest;
+
 import org.apache.commons.io.IOUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
 
+/**
+ * Component which uses Vaadin's built-in Upload component for uploading files.
+ * Allows uploading and removing multiple files. The files are displayed along 
+ * with a potential preview image above the upload button. For each uploaded file, 
+ * a {@link UploadFileRequest} is pepared, but not sent. Those requests can be
+ * accessed via {@link #getUploadFileRequests()} and should be executed in a
+ * later process, e.g. before sending a message. The execution can also be done by
+ * FileProcessorView, using {@link #uploadAllFilesToSmServer()}.
+ * <br/><br/>Warning: Every uploaded file is stored in memory until either it's uploaded
+ * to the Schulmanager server or the file list gets cleared.
+ */
+@SuppressWarnings("serial")
 @PageTitle("File-Processor")
 @Route(value="file-processor", layout= MainLayout.class)
 @CssImport(value = "themes/zitate-sammlung/file-viewer.css")
 public class FileProcessorView extends VerticalLayout {
 
+	private ArrayList<UploadFileRequest> uploadRequests;
+	
+	private VerticalLayout fileList;
+	
+	/**
+	 * Constructor which creates the Upload component and the file list;
+	 * directly usable as functionality is added by the constructor.
+	 */
     public FileProcessorView() {
+    	setSpacing(false);
+    	setWidth("50%");
+    	setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+    	
         MemoryBuffer buffer = new MemoryBuffer();
         Upload upload = new Upload(buffer);
-        upload.setUploadButton(new Button("Upload File"));
         upload.setMaxFiles(5);
         upload.setMaxFileSize(67108864);
+        upload.setI18n(ChatDetailView.getUploadI18n());
+        
+        fileList = new VerticalLayout();
+        fileList.setSpacing(false);
+        fileList.setWidth("90%");
+        fileList.setMaxWidth("372.78px"); //== max width of the upload component
+        fileList.getStyle()
+        	.setPaddingLeft("8px").setPaddingRight("8px").setPaddingBottom("0");
+        fileList.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        
+        uploadRequests = new ArrayList<UploadFileRequest>();
 
         upload.addSucceededListener(event -> {
+        	UploadFileRequest request = new UploadFileRequest(event.getFileName(), event.getMIMEType(), buffer.getInputStream());
+        	uploadRequests.add(request);
+        	
+        	upload.clearFileList();
+        	
             String mimeType = event.getMIMEType();
             String originalFileName = event.getFileName();
             InputStream inputStream = buffer.getInputStream();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-            byte[] bufferArray = new byte[8192]; // Puffergröße wählen
+            byte[] bufferArray = new byte[8192]; // Buffer size = 8 KiB
             int bytesRead;
             try {
                 while ((bytesRead = inputStream.read(bufferArray)) != -1) {
@@ -44,21 +93,101 @@ public class FileProcessorView extends VerticalLayout {
                 }
                 byte[] processedFileBytes = outputStream.toByteArray();
 
-                // Erstellen der Data-URL basierend auf dem MIME-Typ
+                // Receiving a data URL by encoding the file to Base64  
                 String base64Data = Base64.getEncoder().encodeToString(processedFileBytes);
                 String dataURL = "data:" + mimeType + ";base64," + base64Data;
 
-                // Erstellen des Download-Links mit der Data-URL
+                HorizontalLayout l = new HorizontalLayout();
+                l.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+                l.setSpacing(false);
+                l.setWidthFull();
+                
+                if (mimeType.startsWith("image")) {
+                	Image imgPreview = new Image(dataURL, originalFileName);
+                	imgPreview.setWidth("20px");
+                	imgPreview.setHeight("20px");
+                	imgPreview.getStyle().setPaddingRight("6px");
+                	l.add(imgPreview);
+                }
+                
+                // Creating the download link
                 Anchor downloadLink = new Anchor();
-                downloadLink.setText("Download " + originalFileName);
+                downloadLink.setText(originalFileName);
                 downloadLink.setHref(dataURL);
                 downloadLink.getElement().setAttribute("download", originalFileName);
-                add(downloadLink); // Füge den Download-Link zum Layout hinzu
+                downloadLink.getStyle().set("white-space", "nowrap");
+                downloadLink.getStyle().set("overflow", "hidden");
+                downloadLink.getStyle().set("text-overflow", "ellipsis");
+                l.add(downloadLink);
+                
+                Button removeButton = new Button(LumoIcon.CROSS.create());
+                removeButton.getStyle().setPaddingLeft("4px");
+                removeButton.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_CONTRAST);
+                removeButton.addClickListener(e -> {
+                	uploadRequests.remove(request);
+                	fileList.remove(l);
+                });
+                l.add(removeButton);
+                
+                fileList.add(l);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
 
-        add(upload);
+        add(fileList, upload);
     }
+    
+    /**
+     * Removes every uploaded file from the file list along with the
+     * respective UploadFileRequests.
+     */
+    public void clearFileList() {
+    	uploadRequests.clear();
+    	fileList.removeAll();
+    }
+    /**
+     * @return Prepared UploadFileRequests of every file currently 
+     * displayed in the file list.
+     */
+    public UploadFileRequest[] getUploadFileRequests() {
+    	return uploadRequests.toArray(new UploadFileRequest[uploadRequests.size()]);
+    }
+    
+    private ArrayList<String> smFileHandles;
+    
+    /**
+     * Uploads every file currently displayed in the file list to the
+     * Schulmanager server. This removes every uploaded file from
+     * the component along with their respective UploadFileRequests.
+     * The server's responses
+     */
+    public void uploadAllFilesToSmServer() {
+    	for (UploadFileRequest r : uploadRequests) {
+    		r.execute();
+    		smFileHandles.add(r.getResponse());
+    	}
+    	clearFileList();
+    }
+    
+    /**
+     * Returns a String array which contains the file handle of every
+     * file which has ever been uploaded to the Schulmanager server by this
+     * component. A file handle is the server's response to an upload.
+     * If {@link #uploadAllFilesToSmServer()} hasn't been called
+     * yet, the method returns <code>null</code>.
+     * @return Array of the server's responses to each upload, or <code>null</code> if nothing 
+     * has been uploaded to the Schulmanager server.
+     */
+    public String[] getSmFileHandles() {
+    	return smFileHandles.toArray(new String[smFileHandles.size()]);
+    }
+    
+    /**
+     * Deletes the file handles of all previously to Schulmanager uploaded files.
+     */
+    public void clearHistory() {
+    	smFileHandles.clear();
+    }
+    
 }

src/main/java/schulmanager/api/UploadFileRequest.java
@@ -1,0 +1,83 @@
+package schulmanager.api;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import service.DataManager;
+
+public class UploadFileRequest {
+    
+    private final String CALL_URL = "https://login.schulmanager-online.de/api/upload-file?scope=messenger&";
+    
+    private String fileName, mimeType, response;
+    private InputStream fileData;
+    
+    public UploadFileRequest(String fileName, String mimeType, InputStream fileData) {
+    	
+    	this.fileName = fileName;
+    	this.mimeType = mimeType;
+    	this.fileData = fileData;
+    	
+    }
+    
+    public void execute() {
+    	
+        try {
+            mimeType = mimeType.replace("/", "%2F");
+            String url = CALL_URL + "name=" + fileName + "&type=" + mimeType;
+
+            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/octet-stream");
+            con.setRequestProperty("Authorization", "Bearer " + DataManager.getSmSession().getAuthToken());
+            con.setDoOutput(true);
+
+            // Write to server
+            try (OutputStream os = con.getOutputStream();
+                 BufferedInputStream bis = new BufferedInputStream(fileData);
+                 BufferedOutputStream bos = new BufferedOutputStream(os)) {
+                
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = bis.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bytesRead);
+                }
+                bos.flush();
+            }
+
+            // Get response from server
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+            
+            try (InputStream is = con.getInputStream();
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                this.response = response.toString();
+                System.out.println("Response after uploading file: " + response.toString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getFileName() {
+    	return fileName;
+    }
+    
+    public String getResponse() {
+    	return response;
+    }
+    
+}

commit 5608d352cc6a285326c7f95f13950a961327c33f

Correcting layout bug inside ChatDetailView caused by commit 4f9ca74

frontend/themes/zitate-sammlung/schulmanager.css
@@ -180,7 +180,7 @@ span.origi, span.origi > b {
   background-color: var(--lumo-base-color);
   position: fixed;
   position: -webkit-fixed;
-  top: 0;
+  top: 15px;
   width: 110%;
   z-index: 10;
   margin-bottom: 10px;

src/main/java/com/abi/quotes/views/chat_detail/ChatDetailView.java
@@ -213,7 +213,7 @@ public class ChatDetailView extends SmView implements HasDynamicTitle, HasUrlPar
 		innerLayout.setSpacing(false);
 		innerLayout.setWidth("100%");
 		innerLayout.setMaxWidth("900px");
-		innerLayout.getStyle().set("padding-top", "18px");
+		innerLayout.getStyle().set("padding-top", "98px");
 		innerLayout.getStyle().set("padding-bottom", "100px");
 		
 		LocalDate currDate = null;
@@ -284,19 +284,19 @@ public class ChatDetailView extends SmView implements HasDynamicTitle, HasUrlPar
 		bubbleLayout.add(senderSpan, messageText);
 
 		if (attachments.size() > 0) {
-			// Copied from BetaTestView
+
 			UI.getCurrent().getPage().retrieveExtendedClientDetails(receiver -> {
 				screenWidth = receiver.getScreenWidth();
-				//System.out.println("Screen width: " + screenWidth);
 
 				for (int i = 0; i < attachments.size(); i++) {
 					bubbleLayout.add(new AttachmentDisplay(attachments.get(i).getAsJsonObject(), screenWidth));
 				}
-
-				bubbleLayout.add(timeSpan);
-				bubbleLayout.setHorizontalComponentAlignment(Alignment.END, timeSpan);
+	
 			});
 		}
+		
+		bubbleLayout.add(timeSpan);
+		bubbleLayout.setHorizontalComponentAlignment(Alignment.END, timeSpan);
 
 		box.add(bubbleLayout);
 		return box;

src/main/java/com/abi/quotes/views/chats/ChatsView.java
@@ -1,4 +1,4 @@
-package com.abi.quotes.view.chats;
+package com.abi.quotes.views.chats;
 
 import com.abi.quotes.views.MainLayout;
 import com.abi.quotes.views.beta_test.SmView;

src/main/java/com/abi/quotes/views/other/PrivacyPolicyView.java
@@ -1,4 +1,4 @@
-package com.abi.quotes.views.announcement;
+package com.abi.quotes.views.other;
 
 
 import java.sql.SQLException;